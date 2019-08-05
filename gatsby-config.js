48edc4bb44e430a264b0ef437d5eeea4f6243e1c
Nobal <nobal@collaaj.com>
2019-08-05 09:29:00 +0530

    version upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `nobalmohan`,
+      instagram: `hookmenswear`
     },
   },
   plugins: [
